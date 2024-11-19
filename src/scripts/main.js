1b44469fdc75211b341471b9360ef86f5b19f27f
douglastony <douglasjorge.github@gmail.com>
2024-11-18 21:31:01 -0300

    Criação da sessão FAQ

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
@@ -11,7 +12,19 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.currentTarget.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(question) {
+    const classe = 'faq__list__item--is-active';
+    const elementoPai = question.target.parentNode;
+
+    elementoPai.classList.toggle(classe); 
+
+}       
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -28,3 +41,4 @@ function hideAll (){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
 } 
+
